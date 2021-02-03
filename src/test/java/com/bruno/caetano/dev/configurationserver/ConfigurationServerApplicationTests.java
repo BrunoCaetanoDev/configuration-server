d0515b2bf48d92e825c29e87276a9be930886ac6
DESKTOP-BVONRVL\BRUNO <brunoacc92@gmail.com>
2021-02-03 13:57:26 +0000

    [development]:[initial commit]

---
+++ src/test/java/com/bruno/caetano/dev/configurationserver/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.caetano.dev.configurationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
